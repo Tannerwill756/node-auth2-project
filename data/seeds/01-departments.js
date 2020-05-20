f525ad30775e0ff5c5e8f04ca87445d9189359bc
Tanner Williams <tannerwill756@gmail.com>
2020-05-20 18:56:26 -0400

    deployment practice

--- data/seeds/01-departments.js
+++ data/seeds/01-departments.js
@@ -1,13 +1,11 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("departments")
-    .truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex("departments").insert([
-        { id: 1, name: "Engineering" },
-        { id: 2, name: "WareHouse" },
-        { id: 3, name: "Contractors" },
-      ]);
-    });
+  return knex("departments").then(function () {
+    // Inserts seed entries
+    return knex("departments").insert([
+      { name: "Engineering" },
+      { name: "WareHouse" },
+      { name: "Contractors" },
+    ]);
+  });
 };
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -21,18 +22,22 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+    client: "pg",
+    connection: connectionString || {
+      host: "localhost",
+      database: "practice",
+      user: "postgres",
+      password: "Fern756",
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 };
